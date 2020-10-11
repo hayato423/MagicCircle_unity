96506930d36523e8a6074afb182f94e921b9ced5
hayato423 <49159244+hayato423@users.noreply.github.com>
2020-10-12 02:44:59 +0900

    UDP通信処理追加

---
+++ Assets/Scripts/UDP.cs
@@ -0,0 +1,94 @@
+﻿using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Socket
+{
+    public class UDP
+    {
+        private UdpClient udpForSend;   //送信用クライアント
+        private string remoteHost = "127.0.0.1"; //送信用IPアドレス
+        private int remotePort;  //送信先のポート
+
+        private UdpClient udpForReceive;    //受信用クライアント
+        public string rcvMsg = "ini";   //受信メッセージ格納
+        private System.Threading.Thread rcvThread;  //受信用スレッド
+
+        public UDP()
+        {
+
+        }
+
+        public bool init(int port_snd, int port_to, int port_rcv)  //UDP設定(送受信用ポートを開きつつ受信用スレッドを生成)
+        {
+            try
+            {
+                udpForSend = new UdpClient(port_snd);  //送信用ポート
+                remotePort = port_to;
+                udpForReceive = new UdpClient(port_rcv);  //受信用ポート
+                rcvThread = new System.Threading.Thread(new System.Threading.ThreadStart(receive));
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void send(string sendMsg)  //文字列を送受信用ポートから送信先ポートに送信
+        {
+            try
+            {
+                byte[] sendBytes = Encoding.ASCII.GetBytes(sendMsg);
+                udpForSend.Send(sendBytes, sendBytes.Length, remoteHost, remotePort);
+            }
+            catch { }
+        }
+
+        public void receive()  //受信スレッドで実行される関数
+        {
+            IPEndPoint remoteEP = null;  //任意の送信元からのデータを受信;
+            while (true)
+            {
+                try
+                {
+                    byte[] rcvBytes = udpForReceive.Receive(ref remoteEP);
+                    Interlocked.Exchange(ref rcvMsg, Encoding.ASCII.GetString(rcvBytes));
+                }
+                catch { }
+            }
+        }
+
+        public void start_receive()
+        {
+            try
+            {
+                rcvThread.Start();
+            }
+            catch { }
+        }
+
+        public void stop_receive()
+        {
+            try
+            {
+                rcvThread.Interrupt();
+            }
+            catch { }
+        }
+
+        public void end()
+        {
+            try
+            {
+                udpForReceive.Close();
+                udpForSend.Close();
+                rcvThread.Abort();
+            }
+            catch { }
+        }
+
+
+    }
+}
--- Assets/Scripts/UDPClient.cs
+++ Assets/Scripts/UDPClient.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Socket;
 
 public class UDPClient : MonoBehaviour
 {
+
+    private UDP client;
+    private string rcvMsg = "";
+    private string pRcvMsg = "";
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        client = new UDP();
+        client.init(5555, 8000, 3000);
+        client.start_receive();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rcvMsg = client.rcvMsg;        
+        if(rcvMsg != pRcvMsg)
+        {
+            Debug.Log(rcvMsg);
+            pRcvMsg = rcvMsg;
+        }
     }
 }
