0154527c342e456d71be3f29cfd5e8cf380b29eb
hayato423 <49159244+hayato423@users.noreply.github.com>
2020-10-12 18:27:24 +0900

    魔法陣のテクスチャ生成

--- Assets/Scripts/UDPClient.cs
+++ Assets/Scripts/UDPClient.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Socket;
+using System;
 
 public class UDPClient : MonoBehaviour
 {
 
     private UDP client;
-    private string rcvMsg = "";
-    private string pRcvMsg = "";
+    private string rcvMsg = "ini";
+    private string pRcvMsg = "ini";
+    private GameObject MagicCircle;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,7 @@ public class UDPClient : MonoBehaviour
         client = new UDP();
         client.init(5555, 8000, 3000);
         client.start_receive();
+        MagicCircle = GameObject.Find("MagicCircle");
     }
 
     // Update is called once per frame
@@ -23,8 +26,12 @@ public class UDPClient : MonoBehaviour
     {
         rcvMsg = client.rcvMsg;        
         if(rcvMsg != pRcvMsg)
-        {
-            Debug.Log(rcvMsg);
+        {            
+            string[] msgArr = rcvMsg.Split(',');
+            string base64 = msgArr[3];
+            int[] parametar = new int[3] { int.Parse(msgArr[0]), int.Parse(msgArr[1]), int.Parse(msgArr[2]) };
+            MagicCircle mc = MagicCircle.GetComponent<MagicCircle>();
+            mc.ChangeTexture(base64);
             pRcvMsg = rcvMsg;
         }
     }
